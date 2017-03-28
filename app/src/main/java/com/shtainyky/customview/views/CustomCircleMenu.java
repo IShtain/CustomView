ad659a56cfd983b4fb2f43ee74060ddbe53cdcd5
Irina Shtain <shtain_irina@ukr.net>
2017-03-28 12:11:12 +0300

    first commit

---
+++ app/src/main/java/com/shtainyky/customview/views/CustomCircleMenu.java
@@ -0,0 +1,16 @@
+package com.shtainyky.customview.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CustomCircleMenu extends View {
+    public CustomCircleMenu(Context context) {
+        super(context);
+    }
+
+    public CustomCircleMenu(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
